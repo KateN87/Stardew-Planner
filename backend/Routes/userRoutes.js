c87fe010bc8746111a715274b40e052905a2050d
Kate Nordqvist <kate.nordqvist@gmail.com>
2023-04-02 16:25:50 +0200

    resourceModels back, resourceList front

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -1,40 +1,5 @@
 import express from 'express';
-import User from '../models/userModel.js';
-import jwt from 'jsonwebtoken';
 
 const router = express.Router();
-
-const createToken = (_id) => {
-    return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '1d' });
-};
-
-//login route
-router.post('/login', async (req, res) => {
-    const { userName, password } = req.body;
-
-    try {
-        const user = await User.login(userName, password);
-
-        const token = createToken(user._id);
-
-        res.status(200).json({ userName, token });
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-});
-
-//Signup route
-router.post('/signup', async (req, res) => {
-    const { userName, password } = req.body;
-    try {
-        const user = await User.signup(userName, password);
-        console.log(user._id);
-        console.log(process.env.SECRET);
-        const token = createToken(user._id);
-        res.status(200).json({ userName, token });
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-});
 
 export default router;
